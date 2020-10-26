970a409f64c0da82811c0a39afd4a130164c765b
akronim <marinko.miskovic.hr@gmail.com>
2020-10-26 10:13:32 +0100

    First commit

---
+++ vue/components/FirstComponent.vue
@@ -0,0 +1,14 @@
+﻿<template>
+    <div>
+        The year is {{ year }}
+    </div>
+</template>
+<script>
+    module.exports = {
+        data() {
+            return {
+                year: new Date().getFullYear()
+            }
+        }
+    }
+</script>
